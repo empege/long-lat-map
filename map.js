e4028f41df69cc7a51978fbd9236ba89b02b2e9d
Ivan <ivan.mitov@hotmail.com>
2023-03-31 16:27:58 +0200

    First part done

--- map.js
+++ map.js
@@ -1,30 +1,24 @@
-const markers = [
+const cities = [
   {
     name: 'Belgrade',
     coords: [44.787197, 20.457273],
-    style: {
-      fill: '#5c5cff'
-    }
+    // style: {
+    //   fill: '#5c5cff'
+    // }
   },
   {
     name: 'Sydney',
     coords: [-33.865143, 151.209900],
-    style: {
-      fill: '#5c5cff'
-    }
   },
   {
     name: 'Rio',
     coords: [-22.908333, -43.196388],
-    style: {
-      fill: '#5c5cff'
-    }
   }
 ];
 
 var map = new jsVectorMap({
   map: 'world',
-  selector: '#map',
+  selector: '.map__root',
   zoomButtons : false,
   zoomOnScroll : false,
   markersSelectable: false,
@@ -34,50 +28,43 @@ var map = new jsVectorMap({
     event.preventDefault()
   },
 
-  labels: {
-    markers: {
-      render: function(marker) {
-        return marker.name
-      },
-    },
-  },
-
   regionStyle: {
     initial: {
-      fill: 'green'
+      fill: 'rgba(150, 209, 254, 0.2)',
+      // stroke: 'rgba(150, 209, 254, 0.2)',
+      // strokeWidth: 1
     },
     hover: {
       fillOpacity: 1
     }
   },
 
-  markers: markers,
+  markers: cities,
   markerStyle: {
-    hover: {
-      stroke: "#DDD",
-      strokeWidth: 3,
-      fill: '#FFF'
+    initial: {
+      stroke: '#96D1FE',
+      strokeWidth: 8,
+      strokeOpacity: '0.4',
+      fill: '#96D1FE',
+      r: '4'
     },
     selected: {
       fill: '#ff525d'
     }
   },
-  markerLabelStyle: {
-    initial: {
-      display: 'none'
-    },
-  },
   onLoaded(map) {
     window.addEventListener('resize', () => {
       map.updateSize()
-      updateMapContainerSize()
   })
 }
 })
 
-const mapEl = document.getElementById('map')
+const mapEl = document.querySelector('.map__root')
 
 const updateMapContainerSize = () => {
   mapEl.style.height = `${mapEl.clientWidth / 2}px`
 }
+
 updateMapContainerSize()
+
+window.addEventListener('resize', updateMapContainerSize)
